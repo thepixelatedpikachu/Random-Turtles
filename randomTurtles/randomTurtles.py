acefb9a92a68876a622d3b1f80a559dfd207043a
John Paul Antonovich <39472411+tacos247@users.noreply.github.com>
2018-05-20 19:02:07 -0700

    Made code object oriented.
    The code is now object oriented.

--- randomTurtles/randomTurtles.py
+++ randomTurtles/randomTurtles.py
@@ -3,23 +3,27 @@ import random
 import turtle
 
 
-tut1 = Turtle()
-tut2 = Turtle()
-tut3 = Turtle()
-tut4 = Turtle()
-tut1.color("red")
-tut2.color("blue")
-tut3.color("yellow")
-tut4.color("green")
+class RandomTurtles():
+    def __init__(self, tut1=None, tut2=None, tut3=None, tut4=None):
+        self.tut1 = Turtle()
+        self.tut2 = Turtle()
+        self.tut3 = Turtle()
+        self.tut4 = Turtle()
 
+    def setColors(self, color1, color2, color3, color4):
+        self.tut1.color(str(color1))
+        self.tut2.color(str(color2))
+        self.tut3.color(str(color3))
+        self.tut4.color(str(color4))
 
-while True:
-    tut1.forward(random.randint(10, 30))
-    tut1.right(random.randint(1, 70))
-    tut2.forward(random.randint(5, 30))
-    tut2.right(random.randint(10, 70))
-    tut3.forward(random.randint(10, 30))
-    tut3.left(random.randint(1, 70))
-    tut4.forward(random.randint(10, 30))
-    tut4.left(random.randint(1, 70))
+    def moveTurtles(self):
+        while True:
+            self.tut1.forward(random.randint(10, 30))
+            self.tut1.right(random.randint(1, 70))
+            self.tut2.forward(random.randint(5, 30))
+            self.tut2.right(random.randint(10, 70))
+            self.tut3.forward(random.randint(10, 30))
+            self.tut3.left(random.randint(1, 70))
+            self.tut4.forward(random.randint(10, 30))
+            self.tut4.left(random.randint(1, 70))
 
